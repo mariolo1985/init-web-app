767e0633eed503430b1fe09175bd218645f6986c
Mario Lo <kinfonglo@overstock.com>
2019-01-10 14:20:58 -0700

    added babel polyfill

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const entries = [
+    'babel-polyfill',
     path.join(__dirname, 'src/js/master.js'),
     path.join(__dirname, 'src/scss/master.scss')
 ];
