17c2bc14a20723569af24e905ff105a2f9e1aceb
Mario Lo <kinfonglo@overstock.com>
2019-02-08 08:47:14 -0700

    adding our test component with hooks

--- src/js/master.js
+++ src/js/master.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { Menu } from '../components';
+import { Menu, Test } from '../components';
 
 (() => {
     const menu = document.getElementById('menu-container');
     if (menu !== null) {
         render(
-            <Menu />,
+            <Test />,
             menu
         );
     }
